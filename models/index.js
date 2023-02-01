e92467c55be4e5a86e80f9aebc14d190b55139b7
huan00 <96999144+huan00@users.noreply.github.com>
2023-02-01 13:04:16 -0500

    update port

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
 'use strict'
-require('dotenv').config()
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(path.join(__dirname + '/../config/config.json'))[env]
+console.log(process.env.NODE_ENV)
+const config = require(__dirname + '/../config/config.json')[env]
+console.log(config)
 const db = {}
 
 let sequelize
